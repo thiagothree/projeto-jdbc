1ceb1e86492fa387d40d789e87c8ceadc15bd3aa
thiagothree <thiagosousa.contato01@gmail.com>
2022-09-16 15:54:43 -0300

    sellerdaojsbc and daofactory

---
+++ projeto-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer obj) {
+
+    }
+
+    @Override
+    public Department findById(Integer obj) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
